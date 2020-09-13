20aa44eaf375f66c097f0b8d635b9db50939217b
Jessi Poulson <jessipoulson@hotmail.com>
2020-09-13 22:14:09 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,13 @@
-#write your code here
+def countdown (number)
+  number = 11
+  while number > 1
+    number -= 1
+    puts "#{number} SECOND(S)!"
+  end
+  puts "HAPPY NEW YEAR!"
+end
 
-def countdown
+
+def countdown_with_sleep
+  
 end
