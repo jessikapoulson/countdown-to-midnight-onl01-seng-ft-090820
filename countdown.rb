d0da11568f1928b4027e30e15caab4ef06d06181
Jessi Poulson <jessipoulson@hotmail.com>
2020-09-13 22:18:56 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -8,11 +8,11 @@ def countdown(number)
 end
 
 
-def countdown_with_sleep
+def countdown_with_sleep(number)
   number = 10
   while number > 0
     puts "#{number} SECOND(S)!"
-    sleep 5
+    sleep 1
      number -= 1
   end
   "HAPPY NEW YEAR!"
